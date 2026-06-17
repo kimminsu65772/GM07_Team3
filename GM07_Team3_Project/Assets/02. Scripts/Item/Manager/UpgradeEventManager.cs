36959a776690b07bcecdeb1109a8480188c2d640
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-17 15:22:14 +0900

    Feat: UpgradeEventManager 구현
    업그레이드가 선택 되었을 때 이벤트를 호출하는 역할 구현

---
+++ GM07_Team3_Project/Assets/02. Scripts/Item/Manager/UpgradeEventManager.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class UpgradeEventManager : MonoBehaviour
+{
+    //업그레이드 선택  이벤트 
+    public static event Action<UpgradeData> OnUpgradeSelected;
+
+    public static void SelectUpgrade(UpgradeData upgradeData)
+    {
+        OnUpgradeSelected?.Invoke(upgradeData);
+    }
+   
+}
