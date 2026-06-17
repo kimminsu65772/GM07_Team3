4b7043b42540777a2eea6346424187631ce01bf9
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-17 14:52:36 +0900

    refactor: 주석 추기
    주석 추가

--- GM07_Team3_Project/Assets/02. Scripts/Item/ItemData/UpgradeDatabase.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Item/ItemData/UpgradeDatabase.cs
@@ -11,21 +11,27 @@ public class UpgradeDatabase : ScriptableObject
 
     public List<UpgradeData>  GetRandomUpgardes(int count)
     {
+        //원본 복사
         List<UpgradeData> copyList = new List<UpgradeData>(upgrades);
+        //결과 리스트
         List<UpgradeData> resault = new List<UpgradeData>();
 
+        //뽑을 카드 개수
         for (int i = 0; i < count; i++)
         {
             if (copyList.Count <= 0)
             {
                 break;
             }
-
+            //랜덤한 능력 or 무기 뽑기
             int randomindex = Random.Range(0, copyList.Count);
 
+            //결과 넣고
             resault.Add(copyList[randomindex]);
+            //중복방지
             copyList.RemoveAt(randomindex);
         }
+        //결과 반환
         return resault;
     }
 }
