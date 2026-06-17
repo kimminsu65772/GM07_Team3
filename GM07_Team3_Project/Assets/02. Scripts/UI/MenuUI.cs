9bdbef73ec4f5600b7bd6e69ce323ae4373c61f8
GM07_KimMinsu <kimminsu65772@gmail.com>
2026-06-17 14:35:15 +0900

    feat: 마우스를 누르고 있는 동안 버튼 애니메이션 구현
    마우스를 누르는 동안, 그리고 그 상태에서 마우스를 떼었을 때 애니메이션을 추가하여 버튼 UI에 생동감을 더하고자 하였습니다.

--- GM07_Team3_Project/Assets/02. Scripts/UI/MenuUI.cs
+++ GM07_Team3_Project/Assets/02. Scripts/UI/MenuUI.cs
@@ -8,7 +8,7 @@ using TMPro;
  * MenuUI는 메뉴 버튼 UI에 애니메이션 효과를 적용하는 스크립트입니다.
  * 마우스와의 상호작용과 버튼 클릭 시 애니메이션 효과를 어떤 방식으로 적용할 것인지 정의합니다.
  */
-public class MenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class MenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     // 애니메이션 타겟이 되는 버튼의 텍스트
     [SerializeField] private TextMeshProUGUI buttonText; 
@@ -16,6 +16,7 @@ public class MenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     // 버튼에 마우스가 올라갔을 때의 스케일을 정의하는 변수
     [Header("Hover Settings")]
     [SerializeField] private float hoverScale = 1.2f;
+    [SerializeField] private float pressScale = 0.9f;
 
     private RectTransform rectTransform;
     private Vector3 originalScale;
@@ -54,5 +55,25 @@ public class MenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         transform.DOKill();
         transform.DOScale(originalScale, 0.2f).SetEase(Ease.OutBack);
         buttonText.DOColor(originalTextColor, 0.2f).SetEase(Ease.OutBack);
+    }
+
+    // 마우스 버튼을 누르는 동안 호출되는 메서드
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isClickable) return;
+        if (!buttonText) return;
+        transform.DOKill();
+        transform.DOScale(originalScale * pressScale, 0.1f).SetEase(Ease.OutBack);
+        buttonText.DOFade(0.5f, 0.1f).SetEase(Ease.OutBack);
+    }
+
+    // 마우스 버튼에서 손을 뗄 때 호출되는 메서드
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!isClickable) return;
+        if (!buttonText) return;
+        transform.DOKill();
+        transform.DOScale(originalScale, 0.1f).SetEase(Ease.OutBack);
+        buttonText.DOFade(1f, 0.1f).SetEase(Ease.OutBack);
     }
 }
