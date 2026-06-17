3d3992c4e74487e869db6b294a37cb624e7e6d76
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-17 19:40:57 +0900

    refactor: 코드 수정

--- GM07_Team3_Project/Assets/02. Scripts/Item/Weapon/WeaponBase.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Item/Weapon/WeaponBase.cs
@@ -1,6 +1,63 @@
 using UnityEngine;
 
+
 public class WeaponBase : MonoBehaviour
 {
-    
+    private UpgradeData upgradeData;
+    private Transform owner;
+    private Transform target;
+
+    private float attackInterval = 1.0f;
+
+    private float timer = 0.0f;
+
+    private void Update()
+    {
+        if(upgradeData == null || owner == null)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime;
+
+        if(timer >= attackInterval )
+        {
+            timer = 0.0f;
+           
+        }
+
+        Attack();
+
+    }
+
+    //총알이 날라가는 방향
+    protected virtual Vector3 GetAttackDirection()
+    {
+        //없으면 앞으로
+        if (owner == null)
+            return Vector3.forward;
+        //없으면 앞으로
+        if (target == null)
+            return owner.forward.normalized;
+
+        Vector3 direction = target.position - owner.position;
+        return direction.normalized;
+    }
+
+    public virtual void Init(UpgradeData upgradeData, Transform owner)
+    {
+        this.upgradeData = upgradeData;
+        this.owner = owner;
+    }
+
+    protected virtual void Attack()
+    {
+        Vector3 direction = GetAttackDirection();
+
+    }
+
+
+
+
+
 }
