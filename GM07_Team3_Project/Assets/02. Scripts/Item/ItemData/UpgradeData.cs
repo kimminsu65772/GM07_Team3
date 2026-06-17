3f77b33ed3d8b5dd0fde94a13830a3e38facc3b2
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-17 13:42:22 +0900

    Feat: Scriptableobject 구현
    실제 카드 하나에 들어갈 업그레이드 정보를 저장하는 Scriptableobject

---
+++ GM07_Team3_Project/Assets/02. Scripts/Item/ItemData/UpgradeData.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = " UpgradeData", menuName ="GameData/Upgrade Data")]
+public class UpgradeData : ScriptableObject
+{
+    [Header("업그레이드 타입")]
+    [SerializeField] private UpgradeType upgradeType;
+
+    [Header("능력치 타입")]
+    [SerializeField] private StateType stateType;
+
+    [Header("이름")]
+    [SerializeField] private string upgradeName;
+
+    [Header("설명")]
+    [TextArea]
+    [SerializeField] private string description;
+
+    [Header("아이콘")]
+    [SerializeField] private Sprite icon;
+
+    [Header("무기 프리펩")]
+    [SerializeField] private GameObject weaponPrefab;
+
+    [Header("투사체 프리펩")]
+    [SerializeField] private GameObject bulletPrefab;
+
+    [Header("수치 값")]
+    [SerializeField] private float value;
+
+
+    //외부에서 사용 가능한 읽기전용 프로퍼티
+    public UpgradeType UpgradeType {  get { return upgradeType; }}
+    public StateType StateType { get {return stateType;} }
+    public string UpgradeName { get {return upgradeName;} }
+    public string Description { get {return description;} } 
+    public Sprite Icon { get {return icon;} }
+    public GameObject WeaponPrefab { get {return weaponPrefab;} }
+    public GameObject BulletPrefab { get { return bulletPrefab;} }
+    public float Value { get {return value;} }  
+
+}
