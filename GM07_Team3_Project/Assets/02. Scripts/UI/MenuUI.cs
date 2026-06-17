9ecde8d379a0a00cf879b628ce4420a2e5157436
GM07_KimMinsu <kimminsu65772@gmail.com>
2026-06-17 13:55:32 +0900

    feat: 메뉴 버튼에 마우스 상호작용 기능 추가
    사용자가 현재 어떤 메뉴를 선택하고 있는지 직관적으로 확인할 수 있도록 마우스 상호작용 기능을 추가하였습니다.
    마우스를 버튼 위에 올리면 크기가 커지고 마우스를 다시 내려놓으면 원상태로 돌아갑니다.

--- GM07_Team3_Project/Assets/02. Scripts/UI/MenuUI.cs
+++ GM07_Team3_Project/Assets/02. Scripts/UI/MenuUI.cs
@@ -1,14 +1,47 @@
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class MenuUI : MonoBehaviour
+/*
+ * MenuUI
+ * MenuUI는 메뉴 버튼 UI에 애니메이션 효과를 적용하는 스크립트입니다.
+ * 마우스와의 상호작용과 버튼 클릭 시 애니메이션 효과를 어떤 방식으로 적용할 것인지 정의합니다.
+ */
+public class MenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    void Start()
+    // 애니메이션 타겟이 되는 버튼의 텍스트
+    [SerializeField] private TextMeshProUGUI buttonText; 
+
+    // 버튼에 마우스가 올라갔을 때의 스케일을 정의하는 변수
+    [Header("Hover Settings")]
+    [SerializeField] private float hoverScale = 1.2f;
+
+    private RectTransform rectTransform;
+    private Vector3 originalScale;
+    private bool isClickable;
+
+    public void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        originalScale = rectTransform.transform.localScale;
+        isClickable = true;
     }
 
-    void Update()
+    // 버튼 위에 마우스가 올라갔을 때 호출되는 메서드
+    public void OnPointerEnter(PointerEventData eventData)
     {
+        if (!isClickable) return;
         
+        // 현재 진행중인 애니메이션이 있다면 중지 
+        transform.DOKill();
+        transform.DOScale(originalScale * hoverScale, 0.2f).SetEase(Ease.OutBack);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isClickable) return;
+        transform.DOKill();
+        transform.DOScale(originalScale, 0.2f).SetEase(Ease.OutBack);
     }
 }
