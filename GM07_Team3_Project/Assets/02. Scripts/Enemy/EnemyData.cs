b40903b575c6aae8645f0f04ea8e2d06348e36b7
NuriOnline <dm623623@gmail.com>
2026-06-18 05:31:38 +0900

    feat: MeleeEnemy 추가
    플레이어 추적, 공격 기능 아직 미구현

--- GM07_Team3_Project/Assets/02. Scripts/Enemy/EnemyData.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Enemy/EnemyData.cs
@@ -1,31 +1,25 @@
 using UnityEngine;
 
 [CreateAssetMenu(
-    fileName = "EnemyData", 
+    fileName = "EnemyData",
     menuName = "Enemy/EnemyData")]
 
 public class EnemyData : ScriptableObject
 {
-    [SerializeField] private float maxHp = 100f; //체력
+    [Header("HP")]
+    [SerializeField] private float maxHp = 100f; //최대 체력
 
+    [Header("Combat")]
     [SerializeField] private float attackPower = 10f; //공격력
-
     [SerializeField] private float defensePower = 3f; //방어력
+    [SerializeField] private float attackSpeed = 1f; //공격속도
 
-    [SerializeField] private float attackSpeed = 1f; //공속
-
-    [SerializeField] private float moveSpeed = 3f; //이속
-
-    [SerializeField] private float attackRange = 1.5f; //공격범위
-
-    [SerializeField] private float detectRange = 10f; //탐지범위
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 3f; //이동속도
 
     public float MaxHp => maxHp;
     public float AttackPower => attackPower;
     public float DefensePower => defensePower;
     public float AttackSpeed => attackSpeed;
     public float MoveSpeed => moveSpeed;
-    public float AttackRange => attackRange;
-    public float DetectRange => detectRange;
-
 }
