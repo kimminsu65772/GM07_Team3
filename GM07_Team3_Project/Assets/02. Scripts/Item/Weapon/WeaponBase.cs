01a4d5377909022a106c5edca3ac3ab06b8007a2
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-18 11:24:19 +0900

    refactor: WeaponBase코드수정

--- GM07_Team3_Project/Assets/02. Scripts/Item/Weapon/WeaponBase.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Item/Weapon/WeaponBase.cs
@@ -3,46 +3,14 @@ using UnityEngine;
 
 public class WeaponBase : MonoBehaviour
 {
-    private UpgradeData upgradeData;
+  private UpgradeData upgradeData;
     private Transform owner;
     private Transform target;
 
-    private float attackInterval = 1.0f;
-
+    [SerializeField] private float attackInterval = 1.0f;
     private float timer = 0.0f;
 
-    private void Update()
-    {
-        if (upgradeData == null || owner == null)
-        {
-            return;
-        }
 
-        timer += Time.deltaTime;
-
-        if (timer >= attackInterval)
-        {
-            timer = 0.0f;
-
-        }
-
-        Attack();
-
-    }
-
-    //총알이 날라가는 방향
-    protected virtual Vector3 GetAttackDirection()
-    {
-        //없으면 앞으로
-        if (owner == null)
-            return Vector3.forward;
-        //없으면 앞으로
-        if (target == null)
-            return owner.forward.normalized;
-
-        Vector3 direction = target.position - owner.position;
-        return direction.normalized;
-    }
 
     public virtual void Init(UpgradeData upgradeData, Transform owner)
     {
@@ -50,13 +18,56 @@ public class WeaponBase : MonoBehaviour
         this.owner = owner;
     }
 
+    private void Update()
+    {
+        if (upgradeData || owner == null)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime;
+        if(timer > attackInterval)
+        {
+            timer = 0.0f;
+            Attack();
+        }
+
+     
+    }
+    
     protected virtual void Attack()
     {
         Vector3 direction = GetAttackDirection();
+        Vector3 attackPosition = GetSpawnPosition(direction);
+
+        GameObject attackObj = Instantiate(upgradeData.BulletPrefab,
+            attackPosition,
+            Quaternion.LookRotation(direction));
+
+        AttackObject attackObject = attackObj.GetComponent<AttackObject>();
+
+        if(attackObject != null ) 
+            {
+            attackObject.Init(upgradeData.Value, direction, 5.0f);
+            }
+
 
     }
 
+    protected virtual Vector3 GetAttackDirection()
+    {
+        //타겟이 없으면 전방에 발사
+        if (target == null)
+            return owner.forward.normalized;
 
+        Vector3 direction = target.position - owner.position;
+        return direction.normalized;
+    }
+
+    protected virtual Vector3 GetSpawnPosition(Vector3 direction)
+    {
+        return owner.position + direction;
+    }
 
 
 
