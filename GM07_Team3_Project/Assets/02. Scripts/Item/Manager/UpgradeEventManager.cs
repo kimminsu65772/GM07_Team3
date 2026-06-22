59f2fe02d13e9c583d0217e979f3959efb693d6c
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-22 11:27:52 +0900

    refactor: UI이벤트 추가구현

--- GM07_Team3_Project/Assets/02. Scripts/Item/Manager/UpgradeEventManager.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Item/Manager/UpgradeEventManager.cs
@@ -1,10 +1,22 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradeEventManager : MonoBehaviour
 {
+    //UI에 보내는 카드 후보 리시트 
+    public static event Action<List<UpgradeData>> OnUpgradeChoicesCreated;
+
     //업그레이드 선택  이벤트 
+    //선택한 카드 1개를 보내는 이벤트
     public static event Action<UpgradeData> OnUpgradeSelected;
+
+
+    public static void CreateUpgradeChoices(List<UpgradeData>upgrades)
+    {
+        OnUpgradeChoicesCreated?.Invoke(upgrades);
+    }
 
     public static void SelectUpgrade(UpgradeData upgradeData)
     {
