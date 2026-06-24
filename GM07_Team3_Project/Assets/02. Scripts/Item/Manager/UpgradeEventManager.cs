0ee341dae7f42bb77b94b51f7477dfcd40c845cd
GM07_KimMinsu <kimminsu65772@gmail.com>
2026-06-24 10:03:54 +0900

    refactor: 주석 수정

--- GM07_Team3_Project/Assets/02. Scripts/Item/Manager/UpgradeEventManager.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Item/Manager/UpgradeEventManager.cs
@@ -7,7 +7,7 @@ public class UpgradeEventManager : Singleton<UpgradeEventManager>
     // UI에 보내는 카드 후보 리시트 
     public event Action<List<UpgradeData>> OnUpgradeChoicesCreated;
 
-    // 업그레이드 선택  이벤트 
+    // 업그레이드 선택  이벤트
     // 선택한 카드 1개를 보내는 이벤트
     public event Action<UpgradeData> OnUpgradeSelected;
 
