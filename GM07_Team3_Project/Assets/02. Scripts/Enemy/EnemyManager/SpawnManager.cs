d20847dcc178de5be7a20865c126ce79b79e8dec
NuriOnline <dm623623@gmail.com>
2026-06-24 17:18:04 +0900

    feat: SpawnManager 적 스폰위치 보정
    플레이어 주변 랜덤 스폰 위치 결정에 NavMesh.SamplePosition을 사용하도록 코드를 수정했습니다

--- GM07_Team3_Project/Assets/02. Scripts/Enemy/EnemyManager/SpawnManager.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Enemy/EnemyManager/SpawnManager.cs
@@ -38,10 +38,21 @@ public class SpawnManager : Singleton<SpawnManager>
     {
         // 반지름 15m 원 내부에서 랜덤 좌표 생성
         Vector2 randomDirection = Random.insideUnitCircle.normalized;
-
-        // 플레이어 주변 적 생성 위치 계산
-        return player.position + 
+        Vector3 targetPosition = player.position + 
             new Vector3(randomDirection.x, 
             0f, randomDirection.y) * spawnRadius;
+
+        // NavMeshHit 결과를 저장하는 변수
+        NavMeshHit hit;
+
+        // 타겟 위치 주변 5m 이내 가장 가까운 NavMesh 바닥 찾기
+        if (NavMesh.SamplePosition(targetPosition, out hit, 5.0f, NavMesh.AllAreas))
+        {
+            // 실제 바닥 좌표를 찾았다면 해당 위치로 반환
+            return hit.position;
+        }
+
+        // 주변에 NavMesh 바닥이 없으면 targetPosition 반환
+        return targetPosition;
     }
 }
