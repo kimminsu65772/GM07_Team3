6c2c642f28c2cf09efb69122ad40e99baeec8b36
GM07_KimMinsu <kimminsu65772@gmail.com>
2026-06-25 13:47:22 +0900

    feat: 체력바 기본 구현

--- GM07_Team3_Project/Assets/02. Scripts/UI/HUD/Player/PlayerHUD.cs
+++ GM07_Team3_Project/Assets/02. Scripts/UI/HUD/Player/PlayerHUD.cs
@@ -3,5 +3,7 @@ using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
+    [Header("Player HUD 세팅")]
     [SerializeField] private Slider expBar;
+    [SerializeField] private Slider hpBar;
 }
