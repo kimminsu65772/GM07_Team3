8a5f9e9e955aafd4cf6f1d5e2585947f977b0fd4
GM07_KimMinsu <kimminsu65772@gmail.com>
2026-06-25 15:41:53 +0900

    feat: hp바, 플레이어 데이터 연결

--- GM07_Team3_Project/Assets/02. Scripts/UI/HUD/Player/PlayerHUD.cs
+++ GM07_Team3_Project/Assets/02. Scripts/UI/HUD/Player/PlayerHUD.cs
@@ -6,4 +6,31 @@ public class PlayerHUD : MonoBehaviour
     [Header("Player HUD 세팅")]
     [SerializeField] private Slider expBar;
     [SerializeField] private Slider hpBar;
+    [SerializeField] PlayerStatController playerStatController;
+
+    private float currentExp = 0;
+    private float requiredExp;
+
+    private float maxHp;
+    private float currentHp;
+
+
+
+    private void Start()
+    {
+        maxHp = playerStatController.GetStat(StatType.MaxHp);
+        currentHp = maxHp;
+        //requiredExp = playerStatController.
+    }
+
+    private void HandleExpBar()
+    {
+
+    }
+
+    private void HandleHpBar()
+    {
+        currentHp = playerStatController.CurrentHealth;
+        hpBar.value = currentHp / maxHp;
+    }
 }
