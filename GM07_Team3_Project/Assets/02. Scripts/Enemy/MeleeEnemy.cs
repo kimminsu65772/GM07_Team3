06999ff8edee3549d5eeecf9228641739a0d12f2
NuriOnline <dm623623@gmail.com>
2026-06-26 11:38:38 +0900

    feat: 근접 공격 들쭉날쭉한 문제 수정
    OnTriggerEnter와 OnTriggerExit에서 어택타이머를 초기화하도록 수정하였습니다.

--- GM07_Team3_Project/Assets/02. Scripts/Enemy/MeleeEnemy.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Enemy/MeleeEnemy.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class MeleeEnemy : Enemy
 {
+    // 공격 쿨타임 계산용 변수
+    private float attackTimer;
+
     private void Update()
     {
         MoveToTarget();
     }
 
-    // 공격 쿨타임 계산용 변수
-    private float attackTimer;
-
     // 공격 범위 안에 플레이어가 있는 동안 공격 수행용 메서드
     private void OnTriggerStay(Collider other)
     {
         // 충돌 물체 태그가 플레이어인지 확인하는 용도
-        if (!other.CompareTag("Player"))
+        if (!other.CompareTag("Player") || enemyData == null)
         {
             return;
         }
@@ -35,6 +35,24 @@ public class MeleeEnemy : Enemy
         }
 
         attackTimer = 0f;
+    }
+
+    // 공격범위 들어올때 타이머 초기화
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            attackTimer = 0f;
+        }
+    }
+
+    // 플레이어가 공격범위 빠질때 타이머 초기화
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            attackTimer = 0f;
+        }
     }
 
 }
