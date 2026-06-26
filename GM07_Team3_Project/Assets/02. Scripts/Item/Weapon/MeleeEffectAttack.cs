039a2a53a2ba8da2319d86019c1f266d90f6e0a1
jh9683kim-commits <jh9683kim@gmail.com>
2026-06-26 10:19:06 +0900

    refactor: 데미지 추가 구현

--- GM07_Team3_Project/Assets/02. Scripts/Item/Weapon/MeleeEffectAttack.cs
+++ GM07_Team3_Project/Assets/02. Scripts/Item/Weapon/MeleeEffectAttack.cs
@@ -5,44 +5,47 @@ public class MeleeEffectAttack : AttackObject
     [Header("근접 공격 판정")]
     [SerializeField] private float radius = 2.0f;
     [SerializeField] private float attackAngle = 180.0f;
-   // [SerializeField] private LayerMask targetLayer; private HashSet<IDamageable>
+    [SerializeField] private LayerMask targetLayer; private HashSet<IDamageable>
 
-    //hitTargets = new HashSet<IDamageable>();
+    hitTargets = new HashSet<IDamageable>();
 
     public override void Init(float damage, Vector3 direction)
     {
-       // base.Init(damage, direction); hitTargets.Clear();
+        base.Init(damage, direction); hitTargets.Clear();
         transform.rotation = Quaternion.LookRotation(direction);
-        //CheckMeleeHit();
+        CheckMeleeHit();
     }
-    //private void CheckMeleeHit()
-    //{
+    private void CheckMeleeHit()
+    {
 
-        //Collider[] hits = Physics.OverlapSphere(transform.position, radius, targetLayer);
-        //{
-        //    foreach (Collider hit in hits)
-        //    {
-        //        Vector3 toTarget =
-        //        hit.transform.position - transform.position; toTarget.y = 0.0f;
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, targetLayer);
+        {
+            foreach (Collider hit in hits)
+            {
+                Vector3 toTarget =
+                hit.transform.position - transform.position; toTarget.y = 0.0f;
 
-        //        Vector3 attackDirection = direction; attackDirection.y = 0.0f;
+                Vector3 attackDirection = direction; attackDirection.y = 0.0f;
 
-        //        float currentAngle = Vector3.Angle(attackDirection, toTarget);
+                float currentAngle = Vector3.Angle(attackDirection, toTarget);
 
-        //        if (currentAngle > attackAngle * 0.5f) { continue; }
+                if (currentAngle > attackAngle * 0.5f) { continue; }
 
-        //       // IDamageable damageable = hit.GetComponentInParent<IDamageable>();
+                IDamageable damageable = hit.GetComponentInParent<IDamageable>();
 
-        //        //if (damageable == null) { continue; }
-        //        //if (hitTargets.Contains(damageable)) { continue; }
+                if (damageable == null) { continue; }
+                if (hitTargets.Contains(damageable)) { continue; }
 
-        //       // hitTargets.Add(damageable); damageable.TakeDamage(damage);
-        //    }
+                hitTargets.Add(damageable); damageable.TakeDamage(damage);
+            }
+        }
+    }
 
 
 
-        private void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red; Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
+
